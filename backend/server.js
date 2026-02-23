2cdfeb87e2221859c071c034b89a7add2dfa3d4b
Nikhil Anand <nikhilanand2202@gmail.com>
2026-02-23 12:03:52 +0530

    add quiz management functionality with routes for retrieving, submitting, and deleting quizzes

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,7 @@ import authRoutes from './routes/authRoutes.js';
 import documentRoutes from './routes/documentRoutes.js';
 import flashcardRoutes from './routes/flashcardRoutes.js';
 import aiRoutes from './routes/aiRoutes.js'
+import quizRoutes from './routes/quizRoutes.js'
 //Es6 module __dirname alternative 
 
 const __filename= fileURLToPath(import.meta.url)
@@ -42,7 +43,7 @@ app.use('/api/auth',authRoutes)
 app.use('/api/documents',documentRoutes)
 app.use('/api/flashcards',flashcardRoutes)
 app.use('/api/ai',aiRoutes)
-
+app.use('/api/quizzes',quizRoutes)
 app.use(errorHandler);
 
 //404 handler 
