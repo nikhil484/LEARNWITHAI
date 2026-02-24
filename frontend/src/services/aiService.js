1ad6962d8e862500b9840512c7b57904336d7cf2
Nikhil Anand <nikhilanand2202@gmail.com>
2026-02-24 12:28:23 +0530

    add AI and auth services with error handling and API paths

---
+++ frontend/src/services/aiService.js
@@ -0,0 +1,80 @@
+import { API_PATHS } from "../utils/apiPath.js";
+import axiosInstance from "../utils/axiosInstance.js";
+
+const generateFlashcards = async (documentId, options) => {
+    try {
+        const response = await axiosInstance.post(API_PATHS.AI.GENERATE_FLASHCARDS, {
+            documentId,
+            options
+        })
+        return response.data
+    } catch (error) {
+        throw error.response?.data || { message: 'Failed to generate flashcards' }
+
+    }
+}
+
+const generateQuiz = async (documentId, options) => {
+    try {
+        const response = await axiosInstance.post(API_PATHS.AI.GENERATE_QUIZ, {
+            documentId,
+            options
+        })
+        return response.data
+    } catch (error) {
+        throw error.response?.data || { message: 'Failed to generate quiz' }
+    }
+}
+
+const generateSummary = async (documentId) => {
+    try {
+        const response = await axiosInstance.post(API_PATHS.AI.GENERATE_SUMMARY, {documentId})
+        return response.data?.data
+    } catch (error) {
+        throw error.response?.data || { message: 'Failed to generate summary' }
+    }
+}
+
+const chat = async(documentId,message) => {
+    try {
+        const response = await axiosInstance.post(API_PATHS.AI.CHAT, {
+            documentId,
+            question:message
+        })
+        return response.data
+    } catch (error) {
+        throw error.response?.data || { message: 'Chat request failed ' }
+    }
+}
+
+const explainConcept= async(documentId,concept)=>{
+    try {
+        const response= await axiosInstance.post(API_PATHS.AI.EXPLAIN_CONCEPT,{documentId,concept})
+        return response.data?.data
+    } catch (error) {
+                throw error.response?.data || { message: 'failed to explain concept ' }
+
+    }
+}
+
+const getChatHistory = async(documentId) => {
+    try {
+        const response = await axiosInstance.post(API_PATHS.AI.GET_CHAT_HISTORY( documentId)
+        )
+        return response.data
+    } catch (error) {
+        throw error.response?.data || { message: 'Failed to fetch chat history' }
+    }
+}
+
+const aiService={
+    generateFlashcards,
+    generateQuiz,
+    generateSummary,
+    chat,
+    explainConcept,
+    getChatHistory,
+
+}
+
+export default aiService
