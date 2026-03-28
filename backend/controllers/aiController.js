d538d0bc1f85b7922cef230667f12f96431f9975
nikhil484 <86187347+nikhil484@users.noreply.github.com>
2026-03-28 22:57:37 +0530

    Update aiController.js

--- backend/controllers/aiController.js
+++ backend/controllers/aiController.js
@@ -2,7 +2,7 @@ import Document from "../models/Document.js"
 import Flashcard from "../models/Flashcard.js"
 import Quiz from "../models/Quiz.js"
 import ChatHistory from '../models/ChatHistory.js'
-import * as geminiService from '../utils/geminiService.js'//“Import the entire file as an object.”
+import * as geminiService from '../utils/geminiService.js'
 import { findRelevantChunks } from "../utils/textChunker.js"
 
 
