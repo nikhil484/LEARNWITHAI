66aa7a43727572b66b13776e26fb89a0449115dc
nikhil484 <86187347+nikhil484@users.noreply.github.com>
2026-03-28 22:52:24 +0530

    Remove comment on express-validator import
    Remove unnecessary comment about express-validator import.

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { body } from 'express-validator';
-//This line imports body, a validation middleware used to validate and sanitize request body data in Express.
+
 import {
     register,
     login,
